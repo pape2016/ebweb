911335b8f1552f065ce87443766b71562141ccff
pape <pape2006@qq.com>
2017-03-15 22:42:08 +0800

    添加springMVC配置文件springMVC-servlet.xml 增加login方法 在web.xml中添加springMVC配置

--- src/main/java/com/xxnw/eb/controller/CustomerController.java
+++ src/main/java/com/xxnw/eb/controller/CustomerController.java
@@ -1,5 +1,13 @@
 package com.xxnw.eb.controller;
 
-public class CustomerController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class CustomerController {
+	@RequestMapping("/login")
+	public String login() {
+		System.out.println("login success");
+		return "/index.jsp";
+	}
 }
