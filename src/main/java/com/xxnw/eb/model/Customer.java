2822b345150a4f9b9fd1dee91035ec60596c3542
admin <admin@admin-PC>
2017-03-16 00:27:05 +0800

    整合hibernate,

--- src/main/java/com/xxnw/eb/model/Customer.java
+++ src/main/java/com/xxnw/eb/model/Customer.java
@@ -1,5 +1,32 @@
 package com.xxnw.eb.model;
 
 public class Customer {
-
+  private int id;
+  private String name;
+  private String address;
+  private int phone;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public int getPhone() {
+	return phone;
+}
+public void setPhone(int phone) {
+	this.phone = phone;
+}
 }
--- src/main/java/com/xxnw/eb/model/Goods.java
+++ src/main/java/com/xxnw/eb/model/Goods.java
@@ -1,5 +1,47 @@
 package com.xxnw.eb.model;
 
-public class Goods {
 
+public class Goods {
+	 private int id;
+	  private String name;
+	  private byte[] pic;
+	  private double price;
+	  private int quantity;//库存数量
+	  private boolean isDel;//是否删除
+	  public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public byte[] getPic() {
+		return pic;
+	}
+	public void setPic(byte[] pic) {
+		this.pic = pic;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public boolean getIsDel() {
+		return isDel;
+	}
+	public void setIsDel(boolean isDel) {
+		this.isDel = isDel;
+	}
 }
